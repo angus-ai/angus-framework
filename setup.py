4d3b664a8ac36f9c4ad6ba86bdf2e637bdbce368
Aurelien Moreau <aurelien.moreau@angus.ai>
2017-01-03 20:14:42 +0100

    Release candidate 5 for 0.0.12

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ __credits__ = ["Aurélien Moreau", "Gwennaël Gâté"]
 __status__ = "Production"
 
 setup(name='angus-framework',
-      version="0.0.12-rc4",
+      version="0.0.12-rc5",
       description='Angus Cloud Framework',
       author=__author__,
       author_email='aurelien.moreau@angus.ai',
